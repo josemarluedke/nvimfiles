d0cae9141837870ab83b7630220a21499beefa2d
Josemar Luedke <josemarluedke@gmail.com>
2021-07-06 21:01:55 -0700

    Remove default configs from compe; disable spell

--- lua/plugins/nvim-compe.lua
+++ lua/plugins/nvim-compe.lua
@@ -1,18 +1,5 @@
 -- Lua config
 require'compe'.setup {
-  enabled = true,
-  autocomplete = true,
-  debug = false,
-  min_length = 1,
-  preselect = 'enable',
-  throttle_time = 80,
-  source_timeout = 200,
-  incomplete_delay = 400,
-  max_abbr_width = 100,
-  max_kind_width = 100,
-  max_menu_width = 100,
-  documentation = true,
-
   source = {
     path = {kind = '   (Path)'},
     buffer = {kind = '   (Buffer)'},
@@ -20,7 +7,7 @@ require'compe'.setup {
     vsnip = {kind = '   (Snippet)'},
     nvim_lsp = {kind = '   (LSP)'},
     nvim_lua = false,
-    spell = {kind = '   (Spell)'},
+    spell = {kind = '   (Spell)', filetypes = {'markdown', 'text'}},
     tags = false,
     vim_dadbod_completion = true,
     ultisnips = {kind = '   (Snippet)'},
