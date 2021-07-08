77e99a7cd657a08b99040b98592da07f6fcfa070
Josemar Luedke <josemarluedke@gmail.com>
2021-07-08 14:28:46 -0700

    Add mappings for windown nav and term

--- lua/key-map.lua
+++ lua/key-map.lua
@@ -1,53 +1,56 @@
 local map = vim.api.nvim_set_keymap
+local opts = {silent = false, noremap = true}
 
 vim.g.mapleader = ','
 
 -- NvimTree
-map('n', '<leader>m', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
+map('n', '<leader>m', ':NvimTreeToggle<CR>', opts)
+map('n', '<leader>n', ':NvimTreeFindFile<CR>', opts)
 
 -- LSP
-map('n', 'K', ':Lspsaga hover_doc<CR>', {noremap = true, silent = true})
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>',
-    {noremap = true, silent = true})
+map('n', 'K', ':Lspsaga hover_doc<CR>', opts)
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 map('n', 'gs', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>',
-    {noremap = true, silent = true})
-map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>',
-    {noremap = true, silent = true})
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>',
-    {noremap = true, silent = true})
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>',
-    {noremap = true, silent = true})
-map('n', 'ca', ':Lspsaga code_action<CR>', {noremap = true, silent = true})
-map('n', '<C-p>', ':Lspsaga diagnostic_jump_prev<CR>',
-    {noremap = true, silent = true})
-map('n', '<C-n>', ':Lspsaga diagnostic_jump_next<CR>',
-    {noremap = true, silent = true})
-map('n', '<leader>r', '<cmd>lua require("lspsaga.rename").rename()<CR>',
-    {noremap = true, silent = true})
+    opts)
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+map('n', 'ca', ':Lspsaga code_action<CR>', opts)
+map('n', '<C-p>', ':Lspsaga diagnostic_jump_prev<CR>', opts)
+map('n', '<C-n>', ':Lspsaga diagnostic_jump_next<CR>', opts)
+map('n', '<leader>r', '<cmd>lua require("lspsaga.rename").rename()<CR>', opts)
 
 -- scroll down hover doc or scroll in definition preview
 map('n', '<Down>',
-    '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(1)<CR>',
-    {noremap = true, silent = true})
+    '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(1)<CR>', opts)
 -- scroll up hover doc
 map('n', '<Up>',
-    '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(-1)<CR>',
-    {noremap = true, silent = true})
+    '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(-1)<CR>', opts)
 
 -- Telescope
-map('n', '<Leader>f', ':Telescope find_files<CR>',
-    {noremap = true, silent = true})
-map('n', '<Leader>g', ':Telescope live_grep<CR>',
-    {noremap = true, silent = true})
-map('n', 't', ':Telescope find_files<CR>', {noremap = true, silent = true})
+map('n', '<Leader>f', ':Telescope find_files<CR>', opts)
+map('n', '<Leader>g', ':Telescope live_grep<CR>', opts)
+map('n', 't', ':Telescope find_files<CR>', opts)
 
 -- Misc
-map('n', '<leader><space>', ':noh<CR>', {noremap = true, silent = true})
+map('n', '<leader><space>', ':noh<CR>', opts)
 
 -- BarBar
-map('n', '<C-s>', ':BufferPick<CR>', {noremap = true, silent = true})
+map('n', '<C-s>', ':BufferPick<CR>', opts)
 
 -- Replace
 map('n', '<leader>s', ':%s//<left>', {noremap = true})
 map('v', '<leader>s', ':s//<left>', {noremap = true})
+
+-- Better window navigation
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+
+-- Terminal mappings
+map('t', '<esc>', '<C-\\><C-n>', opts)
+map('t', '<C-h>', '<C-\\><C-n><C-w>h', opts)
+map('t', '<C-j>', '<C-\\><C-n><C-w>j', opts)
+map('t', '<C-k>', '<C-\\><C-n><C-w>k', opts)
+map('t', '<C-l>', '<C-\\><C-n><C-w>l', opts)
