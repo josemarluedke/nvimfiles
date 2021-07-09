1d0f7d8ad8edb9851b946e7de27b9fea98ae2d81
Josemar Luedke <josemarluedke@gmail.com>
2021-07-08 21:04:25 -0700

    use cmd instead of column in maps

--- lua/key-map.lua
+++ lua/key-map.lua
@@ -4,20 +4,21 @@ local opts = {silent = false, noremap = true}
 vim.g.mapleader = ','
 
 -- NvimTree
-map('n', '<leader>m', ':NvimTreeToggle<CR>', opts)
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', opts)
+map('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', opts)
+map('n', '<leader>m', '<cmd>NvimTreeToggle<CR>', opts) -- to be removed
+map('n', '<leader>n', '<cmd>NvimTreeFindFile<CR>', opts)
 
 -- LSP
-map('n', 'K', ':Lspsaga hover_doc<CR>', opts)
+map('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts)
 map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 map('n', 'gs', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>',
     opts)
 map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-map('n', 'ca', ':Lspsaga code_action<CR>', opts)
-map('n', '<C-p>', ':Lspsaga diagnostic_jump_prev<CR>', opts)
-map('n', '<C-n>', ':Lspsaga diagnostic_jump_next<CR>', opts)
+map('n', 'ca', '<cmd>Lspsaga code_action<CR>', opts)
+map('n', '<C-p>', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+map('n', '<C-n>', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 map('n', '<leader>r', '<cmd>lua require("lspsaga.rename").rename()<CR>', opts)
 
 -- scroll down hover doc or scroll in definition preview
@@ -28,15 +29,15 @@ map('n', '<Up>',
     '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(-1)<CR>', opts)
 
 -- Telescope
-map('n', '<Leader>f', ':Telescope find_files<CR>', opts)
-map('n', '<Leader>g', ':Telescope live_grep<CR>', opts)
-map('n', 't', ':Telescope find_files<CR>', opts)
+map('n', '<Leader>f', '<cmd>Telescope find_files<CR>', opts)
+map('n', '<Leader>g', '<cmd>Telescope live_grep<CR>', opts)
+map('n', 't', '<cmd>Telescope find_files<CR>', opts)
 
 -- Misc
 map('n', '<leader><space>', ':noh<CR>', opts)
 
 -- BarBar
-map('n', '<C-s>', ':BufferPick<CR>', opts)
+map('n', '<C-s>', '<cmd>BufferPick<CR>', opts)
 
 -- Replace
 map('n', '<leader>s', ':%s//<left>', {noremap = true})
