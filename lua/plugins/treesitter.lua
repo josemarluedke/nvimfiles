775a740fad78029fa4826dd7d8508fe216406ae7
Josemar Luedke <josemarluedke@gmail.com>
2022-09-30 18:49:38 -0700

    Disable phpdoc treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
   ensure_installed = 'all',
-  ignore_install = { 'haskell' },
+  ignore_install = { 'haskell', 'phpdoc' },
   highlight = { enable = true },
   indent = { enable = true },
   autotag = { enable = true }, -- requires windwp/nvim-ts-autotag
