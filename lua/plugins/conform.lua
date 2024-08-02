98c3c47233aefa5980ee814befc9dd85df20401f
Josemar Luedke <josemarluedke@gmail.com>
2024-08-02 12:44:43 -0700

    stuff

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,8 +1,18 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+
+    log_level = vim.log.levels.DEBUG,
+    -- add your config here
     formatters_by_ft = {
-      go = { "gofmt", "goimports" },
+      go = {
+        "gofmt",
+        "goimports",
+        -- "goimports-reviser",
+      },
     },
+    format_on_save = function(bufnr)
+      return { async = true, timeout_ms = 5000, lsp_fallback = true }
+    end,
   },
 }
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,15 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      protols = {},
+    },
+
+    setup = {
+
+      protols = function(_, opts)
+        require("lspconfig").protols.setup({})
+      end,
+    },
+  },
+}
